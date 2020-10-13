4f0ab6c6a5818e9739c0ce34a8c1b036d96a6021
kumiho <konstantin.kim.mail@gmail.com>
2020-10-13 13:53:24 +0300

    start PalindromicFactor solution

---
+++ YandexCup.PalindromicFactor.Test/PalindromeFinderTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace YandexCup.PalindromicFactor.Test
+{
+    [TestFixture]
+    public class PalindromeFinderTest
+    {
+        [TestCase("abac", "aba")]
+        [TestCase("yandex", null)]
+        [TestCase("abbadfs", "bb")]
+        [TestCase("abbadfsaa", "aa")]
+        [TestCase("abbadfsaba", "bb")]
+        public void FindSmallestTest(string inputString, string resultString)
+        {
+            var finder = new PalindromeFinder();
+            Assert.AreEqual(resultString, finder.FindSmallest(inputString));
+        }
+
+        [TestCase("abba")]
+        [TestCase("aba")]
+        [TestCase("aa")]
+        [TestCase("abzzzzzzzba")]
+        public void IsPalindromeTrueTest(string inputString)
+        {
+            var finder = new PalindromeFinder();
+            Assert.IsTrue(finder.IsPalindrome(inputString));
+        }
+
+        [TestCase("avba")]
+        [TestCase("abdavcdba")]
+        [TestCase("bas")]
+        [TestCase("a")]
+        [TestCase("")]
+        public void IsPalindromeFalseTest(string inputString)
+        {
+            var finder = new PalindromeFinder();
+            Assert.IsFalse(finder.IsPalindrome(inputString));
+        }
+    }
+}
---
+++ YandexCup.PalindromicFactor/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YandexCup.PalindromicFactor
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputString = Console.ReadLine();
+            var finder = new PalindromeFinder();
+            var smallestPalindrome = finder.FindSmallest(inputString);
+            
+            Console.Write(string.IsNullOrWhiteSpace(smallestPalindrome)
+                ? "-1"
+                : smallestPalindrome);
+        }
+    }
+
+    public class PalindromeFinder
+    {
+        public string FindSmallest(string text)
+        {
+            var allPalindrome = FindAllPalindrome(text);
+            if (!allPalindrome.Any())
+            {
+                return null;
+            }
+
+            return allPalindrome.OrderBy(x => x.Length)
+                .ThenBy(x => x)
+                .First();
+        }
+
+        private string[] FindAllPalindrome(string inputString)
+        {
+            var allSubstrings = new List<string>();
+            for (int length = 1; length < inputString.Length; length++)
+            {
+                // End index is tricky.
+                for (int start = 0; start <= inputString.Length - length; start++)
+                {
+                    string substring = inputString.Substring(start, length);
+                    allSubstrings.Add(substring);
+                }
+            }
+
+            return allSubstrings.Where(IsPalindrome)
+                .ToArray();
+        }
+
+        public bool IsPalindrome(string inputText)
+        {
+            if (string.IsNullOrWhiteSpace(inputText))
+                return false;
+            
+            if (inputText.Length <= 1)
+                return false;
+
+            var lastSymbolIndex = inputText.Length - 1;
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                if (inputText[i] != inputText[lastSymbolIndex]) 
+                    return false;
+
+                if (lastSymbolIndex <= i)
+                    return true;
+
+                lastSymbolIndex -= 1;
+            }
+
+            return false;
+        }
+    }
+}
