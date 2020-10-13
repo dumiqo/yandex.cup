805ce89ff2b677804850569bd3b3aaf00aaf24fd
kumiho <konstantin.kim.mail@gmail.com>
2020-10-13 13:15:24 +0300

    add LotteryTicket solution

---
+++ YandexCup.LotteryTicket.Test/LotteryTicketCheckerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection.Emit;
+using NUnit.Framework;
+
+namespace YandexCup.LotteryTicket.Test
+{
+    [TestFixture]
+    public class LotteryTicketCheckerTest
+    {
+        [TestCase(new int[] { 1, 2, 3, 4 ,5 ,6 ,7 }, new int[] { 1, 2, 3, 4 ,5 ,6 ,7 })]
+        [TestCase(new int[] { 1, 2, 3, 4 ,5 ,6 ,7 }, new int[] { 1, 3, 32, 12 ,11 ,6 ,7 })]
+        [TestCase(new int[] { 1, 2, 3, 4 ,5 ,6 ,7 }, new int[] { 12, 2, 22, 4 ,5 ,32 ,12 })]
+        public void WinnerNimbersCheckTest(int[] winNumbers, int[] ticketNumbers)
+        {
+            var checker = new LotteryTicketChecker(winNumbers);
+            Assert.IsTrue(checker.IsWinTicket(ticketNumbers));
+        }
+        
+        [TestCase(new int[] { 22, 3 ,4 ,5 ,6 }, new int[] { 22, 3, 4, 21, 21})]
+        public void NotWinnerNimbersCheckTest(int[] winNumbers, int[] ticketNumbers)
+        {
+            var checker = new LotteryTicketChecker(winNumbers);
+            Assert.IsFalse(checker.IsWinTicket(ticketNumbers));
+        }
+    }
+}
---
+++ YandexCup.LotteryTicket/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace YandexCup.LotteryTicket
+{
+    class Program
+    {
+        static void Main()
+        {
+            var winNumbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            var checker = new LotteryTicketChecker(winNumbers);
+            
+            var ticketsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < ticketsCount; i++)
+            {
+                var ticketNumbers = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+                
+                Console.WriteLine(checker.IsWinTicket(ticketNumbers) ? "Lucky" : "Unlucky");
+            }
+        }
+    }
+
+    public class LotteryTicketChecker
+    {
+        private int[] _winNumbers;
+        public LotteryTicketChecker(int[] winNumbers)
+        {
+            _winNumbers = winNumbers.Distinct().ToArray();
+        }
+
+        public bool IsWinTicket(int[] ticketNumbers)
+        {
+            return ticketNumbers
+                       .Distinct()
+                       .Concat(_winNumbers)
+                       .GroupBy(x => x)
+                       .Count(x => Enumerable.Count<int>(x) >= 2)
+                   >= 3;
+        }
+    }
+}
